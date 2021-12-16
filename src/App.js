cc973096fabfea737ba62ca6d54a61d9d1eaa166
ibrokhim <mr017programmer@gmail.com>
2021-12-16 10:26:48 +0500

    first ocmmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import img from "./react.png";
+import "./app.scss";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useScrollPosition = () => {
+  const [scrollYPos, setScrollYPos] = useState(window.pageYOffset);
+
+  const onScroll = () => {
+    setScrollYPos(window.pageYOffset)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.RemoveEventListener("scroll", onScroll);
+    }
+  }, [])
+
+  return scrollYPos
 }
 
-export default App;
+
+export default () => {
+  const scrollPos = useScrollPosition();
+
+  const imgStyle = {
+    transform: `translate3d(-50%, calc(-${scrollPos * .5}px - 50%), 0)`,
+    // filter: `hue-rotate(${scrollPos * 1}deg)`
+  }
+
+  const heading1 = {
+    transform: `translate3d(-${scrollPos / 2}px, -${scrollPos * 0.5}px, 0px)`
+  }
+
+  const heading2 = {
+    transform: `translate3d(${scrollPos / 2}px, -${scrollPos * 0.5}px, 0px)`
+  }
+
+  return (
+    <>
+      <div className="page-wrapper">
+        <div className="text-parent">
+          <h1 style={heading1}>
+            Ibrokhim Ismoilov Ibrokhim Ismoilov Ibrokhim Ismoilov Ibrokhim Ismoilov
+          </h1>
+          <h1 style={heading2}>
+            Frontend Developer Frontend Developer Frontend Developer Frontend Developer
+          </h1>
+        </div>
+        <img src={img} alt="" style={imgStyle} />
+        <div className="text-parent">
+          <h1 className="outline" style={heading1}>
+            Ibrokhim Ismoilov Ibrokhim Ismoilov Ibrokhim Ismoilov Ibrokhim Ismoilov
+          </h1>
+          <h1 className="outline" style={heading2}>
+            Frontend Developer Frontend Developer Frontend Developer Frontend Developer
+          </h1>
+        </div>
+      </div>
+      <div className="section"></div>
+    </>
+  );
+}
